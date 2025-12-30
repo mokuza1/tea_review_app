66397996b5addd06306b8681a7cfcc19244875de
mokuza1e <karen239wo@outlook.jp>
2025-12-30 17:51:10 +0900

    style: RuboCopの指摘に従いフォーマット修正

--- Gemfile
+++ Gemfile
@@ -43,7 +43,7 @@ gem "thruster", require: false
 gem "image_processing", "~> 1.2"
 
 # devise
-gem 'devise'
+gem "devise"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   # 新規登録時のストロングパラメータに「nameカラム」の追加
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
   end
 end
