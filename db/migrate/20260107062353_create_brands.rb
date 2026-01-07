56b357b97921c079e4bcba68986442105635c594
mokuza1e <karen239wo@outlook.jp>
2026-01-07 16:49:32 +0900

    feat:マイグレーションに制約を追記、マイグレーションを実行

--- db/migrate/20260107062353_create_brands.rb
+++ db/migrate/20260107062353_create_brands.rb
@@ -2,12 +2,16 @@ class CreateBrands < ActiveRecord::Migration[8.1]
   def change
     create_table :brands do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :approved_by, null: false, foreign_key: true
+
+      # 承認者（未承認を許容する）
+      t.references :approved_by, null: true,
+                   foreign_key: { to_table: :users }
+
       t.string :name_ja
       t.string :name_en
       t.string :country
       t.text :description
-      t.integer :status
+      t.integer :status, null: false, default: 0
       t.datetime :approved_at
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_30_054350) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_07_062353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.datetime "approved_at"
+    t.bigint "approved_by_id"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name_en"
+    t.string "name_ja"
+    t.integer "status", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["approved_by_id"], name: "index_brands_on_approved_by_id"
+    t.index ["user_id"], name: "index_brands_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,4 +42,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_30_054350) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "brands", "users"
+  add_foreign_key "brands", "users", column: "approved_by_id"
 end
