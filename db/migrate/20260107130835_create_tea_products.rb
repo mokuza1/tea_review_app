a1b85ce5a5236fa4091e98665b1cf98e70a6e73a
mokuza1e <karen239wo@outlook.jp>
2026-01-08 15:29:22 +0900

    feat:マイグレーションファイル編集、実行

--- db/migrate/20260107130835_create_tea_products.rb
+++ db/migrate/20260107130835_create_tea_products.rb
@@ -2,13 +2,13 @@ class CreateTeaProducts < ActiveRecord::Migration[8.1]
   def change
     create_table :tea_products do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :approved_by, null: false, foreign_key: true
-      t.references :brand, null: false, foreign_key: true
+      t.references :approved_by, null: true, foreign_key: { to_table: :users }
+      t.references :brand, null: true, foreign_key: true
       t.string :name
       t.integer :tea_type
       t.integer :caffeine_level
       t.text :description
-      t.integer :status
+      t.integer :status, null: false, default: 0
       t.datetime :approved_at
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_07_062353) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_07_130835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,6 +29,23 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_07_062353) do
     t.index ["user_id"], name: "index_brands_on_user_id"
   end
 
+  create_table "tea_products", force: :cascade do |t|
+    t.datetime "approved_at"
+    t.bigint "approved_by_id"
+    t.bigint "brand_id"
+    t.integer "caffeine_level"
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.integer "status", default: 0, null: false
+    t.integer "tea_type"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["approved_by_id"], name: "index_tea_products_on_approved_by_id"
+    t.index ["brand_id"], name: "index_tea_products_on_brand_id"
+    t.index ["user_id"], name: "index_tea_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -45,4 +62,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_07_062353) do
 
   add_foreign_key "brands", "users"
   add_foreign_key "brands", "users", column: "approved_by_id"
+  add_foreign_key "tea_products", "brands"
+  add_foreign_key "tea_products", "users"
+  add_foreign_key "tea_products", "users", column: "approved_by_id"
 end
