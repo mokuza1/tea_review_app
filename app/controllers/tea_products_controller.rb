96eafc0faff92aba7bb10166bc4adf4a337c3159
mokuza1e <karen239wo@outlook.jp>
2026-01-12 17:55:33 +0900

    feat:コントローラーにshowアクション追加

--- app/controllers/tea_products_controller.rb
+++ app/controllers/tea_products_controller.rb
@@ -2,4 +2,8 @@ class TeaProductsController < ApplicationController
   def index
     @tea_products = TeaProduct.published.includes(:brand).order(created_at: :desc)
   end
+
+  def show
+    @tea_product = TeaProduct.published.includes(:brand).find(params[:id])
+  end
 end
