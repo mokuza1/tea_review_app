fad181f9bd31fe9cb25ca56c095b2c4301255cb2
mokuza1e <karen239wo@outlook.jp>
2026-01-13 22:49:54 +0900

    update:FlavorCategoryマイグレーションファイルを修正

--- db/migrate/20260113130919_create_flavor_categories.rb
+++ db/migrate/20260113130919_create_flavor_categories.rb
@@ -1,9 +1,11 @@
 class CreateFlavorCategories < ActiveRecord::Migration[8.1]
   def change
     create_table :flavor_categories do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
+
+    add_index :flavor_categories, :name, unique: true
   end
 end
