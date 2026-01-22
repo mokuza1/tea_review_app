9c2d0476e3feb318f1346cc1b022e8dda4eb5538
mokuza1e <karen239wo@outlook.jp>
2026-01-22 16:15:51 +0900

    feat:管理画面ダッシュボード編集

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < Admin::BaseController
   def index
-    @pending_tea_products = TeaProduct.pending.count
-    @pending_brands = Brand.pending.count
+    @pending_tea_products_count = TeaProduct.pending.count
+    @pending_brands_count = Brand.pending.count
+    @published_tea_products_count = TeaProduct.published.count
+    @users_count = User.count
   end
 end
