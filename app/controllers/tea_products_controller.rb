8ccfa3a178737682dc8dd0204d81e40d70d4acac
mokuza1e <karen239wo@outlook.jp>
2026-02-09 17:39:30 +0900

    fix:詳細でフレーバー表示の際に出るBullet警告解消

--- app/controllers/tea_products_controller.rb
+++ app/controllers/tea_products_controller.rb
@@ -149,7 +149,7 @@ class TeaProductsController < ApplicationController
   def set_tea_product
     @tea_product = TeaProduct
       .viewable_by(current_user)
-      .includes(:brand, :purchase_locations, :flavors)
+      .includes(:brand, :purchase_locations, flavors: :flavor_category)
       .find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to tea_products_path, alert: "商品が見つかりませんでした"
