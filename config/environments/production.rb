f92c63a5a8156e9861bcd73ede73db59c6935125
mokuza1e <karen239wo@outlook.jp>
2026-03-02 16:50:49 +0900

    feat: 独自ドメイン設定追加、https設定追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
@@ -87,4 +87,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "kocha-zukan.com"
+  config.hosts << "www.kocha-zukan.com"
 end
