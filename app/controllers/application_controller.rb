c7e328b18e1e5d5835f1cfefc6ff49e2c9995c93
mokuza1e <karen239wo@outlook.jp>
2026-03-08 16:02:24 +0900

    feat: ログイン後に元のページに戻るように編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :store_location
   before_action :configure_permitted_parameters, if: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   def after_sign_in_path_for(resource)
-    if resource.admin?
-      admin_root_path
-    else
-      root_path
+    return admin_root_path if resource.admin?
+
+    stored_location_for(resource) || root_path
+  end
+
+  private
+
+  def store_location
+    if request.get? && !devise_controller?
+      store_location_for(:user, request.fullpath)
     end
   end
 
