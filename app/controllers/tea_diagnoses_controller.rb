282f723111ba00753df4b2e85ca32f1463b58298
mokuza1e <karen239wo@outlook.jp>
2026-03-09 01:41:32 +0900

    feat: 診断ビュー実装、コントローラー修正

--- app/controllers/tea_diagnoses_controller.rb
+++ app/controllers/tea_diagnoses_controller.rb
@@ -1,7 +1,8 @@
 class TeaDiagnosesController < ApplicationController
 
   def start
-    session[:scores] = Hash.new(0)
+    session[:scores] = {}
+    session[:step] = 1
     redirect_to question_tea_diagnosis_path(step: 1)
   end
 
@@ -11,16 +12,27 @@ class TeaDiagnosesController < ApplicationController
   end
 
   def answer
+
+    # 未選択対策
+    unless params[:category]
+      redirect_back fallback_location: question_tea_diagnosis_path(step: session[:step]),
+                    alert: "選択してください"
+      return
+    end
+
     category = params[:category].to_sym
+
+    session[:scores][category] ||= 0
     session[:scores][category] += 1
 
-    next_step = session[:scores].values.sum + 1
+    session[:step] += 1
 
-    if TeaDiagnosisService.finished?(next_step)
+    if session[:step] > TeaDiagnosisService.total_questions
       redirect_to result_tea_diagnosis_path
     else
-      redirect_to question_tea_diagnosis_path(step: next_step)
+      redirect_to question_tea_diagnosis_path(step: session[:step])
     end
+
   end
 
   def result
