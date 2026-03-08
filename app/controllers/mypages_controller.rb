99a9927836ebefa3d6fa4e0ec360551c29fda5f1
mokuza1e <karen239wo@outlook.jp>
2026-03-08 14:33:37 +0900

    feat: マイページから自分の投稿とお気に入りを確認できるように編集

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,10 +2,29 @@ class MypagesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @tea_products =
+    @recent_tea_products =
       current_user
         .tea_products
         .includes(:image_attachment)
         .order(created_at: :desc)
+        .limit(3)
+    @recent_favorite_tea_products = 
+      current_user
+        .favorite_tea_products
+        .includes(:image_attachment)
+        .order(created_at: :desc)
+        .limit(3)
+  end
+
+  # 自分の投稿一覧ページ
+  def my_tea_products
+    @tea_products =
+      current_user.tea_products.includes(:image_attachment).order(created_at: :desc)
+  end
+
+  # お気に入り一覧ページ
+  def favorites
+    @favorite_tea_products =
+      current_user.favorite_tea_products.includes(:image_attachment).order(created_at: :desc)
   end
 end
