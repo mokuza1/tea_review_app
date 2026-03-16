1b83bc14c026aac8f955b658c1c197e48b1a419c
mokuza1e <karen239wo@outlook.jp>
2026-03-16 15:39:34 +0900

    feat: 制約追加、二重レビューのエラーメッセージ追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,13 +2,17 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :tea_product
 
-  validates :overall_rating, inclusion: { in: 1..5 }
-  validates :aroma_rating, inclusion: { in: 1..5 }
-  validates :bitterness_rating, inclusion: { in: 1..5 }
-  validates :strength_rating, inclusion: { in: 1..5 }
-  validates :sweetness_rating, inclusion: { in: 1..5 }
+  validates :overall_rating, presence: true, inclusion: { in: 1..5 }
+  validates :aroma_rating, presence: true, inclusion: { in: 1..5 }
+  validates :bitterness_rating, presence: true, inclusion: { in: 1..5 }
+  validates :strength_rating, presence: true, inclusion: { in: 1..5 }
+  validates :sweetness_rating, presence: true, inclusion: { in: 1..5 }
 
   validates :comment, length: { maximum: 140 }
 
-  validates :user_id, uniqueness: { scope: :tea_product_id }
+  validates :user_id,
+    uniqueness: {
+      scope: :tea_product_id,
+      message: "この紅茶にはすでにレビューしています"
+    }
 end
