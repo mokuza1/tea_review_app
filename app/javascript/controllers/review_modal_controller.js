87aa7ad859f333339e06d8191aba0979771055a1
mokuza1e <karen239wo@outlook.jp>
2026-03-18 15:29:21 +0900

    feat: エラー時の自動ラッピングを無効化

--- app/javascript/controllers/review_modal_controller.js
+++ app/javascript/controllers/review_modal_controller.js
@@ -1,11 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
+  // 背景クリックでモーダルを閉じる
   close(event) {
+    // クリックされたのが背景（このコントローラーが付与された要素自身）であるか確認
     if (event.target === this.element) {
-      const frame = document.getElementById("review_modal")
-      frame.innerHTML = ""
+      this.element.closest("turbo-frame").src = null // 接続先をクリア
+      this.element.remove() // 要素自体を削除して完全に壁を取り払う
     }
   }
 
