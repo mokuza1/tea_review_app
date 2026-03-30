2dce63b88eac6c26d3d83d9158658c5f5b9075ce
mokuza1e <karen239wo@outlook.jp>
2026-03-30 23:23:12 +0900

    feat: TeaProductSubmission導入につき編集

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,12 +2,23 @@ class MypagesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @recent_tea_products =
+    submissions =
+      current_user
+        .tea_product_submissions
+        .includes(:image_attachment)
+
+    products =
       current_user
         .tea_products
         .includes(:image_attachment)
-        .order(created_at: :desc)
-        .limit(3)
+
+    combined =
+      (submissions.to_a + products.to_a)
+        .sort_by(&:created_at)
+        .reverse
+
+    @recent_posts = combined.first(3)
+
     @recent_favorite_tea_products =
       current_user
         .favorite_tea_products
@@ -15,14 +26,27 @@ class MypagesController < ApplicationController
         .order(created_at: :desc)
         .limit(3)
 
-    @tea_products_count = current_user.tea_products.count
+    @posts_count = combined.size
     @favorites_count = current_user.favorite_tea_products.count
   end
 
   # 自分の投稿一覧ページ
   def my_tea_products
-    @tea_products =
-      current_user.tea_products.includes(:image_attachment).order(created_at: :desc)
+    submissions =
+      current_user
+        .tea_product_submissions
+        .includes(:image_attachment)
+
+    products =
+      current_user
+        .tea_products
+        .includes(:image_attachment)
+
+  # 配列として結合して日付順に並べる
+    @posts =
+      (submissions.to_a + products.to_a)
+        .sort_by(&:created_at)
+        .reverse
   end
 
   # お気に入り一覧ページ
