fadbf254376ed3ae0ee7be09020bdc02cc5d7954
mokuza1e <karen239wo@outlook.jp>
2026-04-04 17:26:03 +0900

    feat: OmniAuth用のロジック追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   enum :role, { general: 0, admin: 1 }
 
@@ -25,4 +26,25 @@ class User < ApplicationRecord
   has_many :approved_tea_products,
            class_name: "TeaProduct",
            foreign_key: :approved_by_id
+
+  def self.from_omniauth(auth)
+    user = find_by(provider: auth.provider, uid: auth.uid)
+
+    user ||= find_or_initialize_by(email: auth.info.email)
+
+    user.assign_attributes(
+      provider: auth.provider,
+      uid: auth.uid,
+      email: auth.info.email
+    )
+
+    if user.new_record?
+      user.password = Devise.friendly_token[0, 20]
+    end
+
+    user.name = auth.info.name if user.respond_to?(:name)
+
+    user.save
+    user
+  end
 end
