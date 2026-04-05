5e0bfaf14abb37ede816f6b43adcd832b55b613d
mokuza1e <karen239wo@outlook.jp>
2026-04-05 15:07:57 +0900

    feat: Brandモデルテスト作成・実行確認

---
+++ spec/factories/brands.rb
@@ -0,0 +1,13 @@
+# spec/factories/brands.rb
+FactoryBot.define do
+  factory :brand do
+    # sequence を使うことで "Tea Type 1", "Tea Type 2" ... と絶対に重複しない値を作れます
+    sequence(:name_ja) { |n| "#{Faker::Tea.type} #{n}ブランド" }
+    sequence(:name_en) { |n| "Tea Brand #{n}" }
+    
+    country { "United Kingdom" }
+    description { "美味しい紅茶のブランドです。" }
+    status { :draft }
+    association :user
+  end
+end
